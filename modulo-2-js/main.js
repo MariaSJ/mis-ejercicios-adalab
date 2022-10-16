f7cf80812f93b88851d8b9b81a246687d3096e33
Maria SJ <maria.som.jur@gmail.com>
2022-10-16 19:57:17 +0200

    modulo 2

---
+++ modulo-2-js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const titleElement = document.querySelector('.title');
+titleElement.innerHTML = titleElement.innerHTML + " mundo";
+
+const subtitleElement = document.querySelector('.subtitle');
+subtitleElement.innerHTML = subtitleElement.innerHTML + 'María Somoza';
+
+const divElement = document.querySelector('.div');
+const content = '<h1>Lorem Ipsum</h1><img src ="http://via.placeholder.com/350x150"/><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>';
+divElement.innerHTML = content;
