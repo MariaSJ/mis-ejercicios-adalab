e27752d294bb01fa33344b2f2d1dde5186e77941
Maria SJ <maria.som.jur@gmail.com>
2022-10-18 10:18:50 +0200

    exercise

--- modulo-2-js/main.js
+++ modulo-2-js/main.js
@@ -1,11 +1,13 @@
 "use strict";
 
-const titleElement = document.querySelector('.title');
-titleElement.innerHTML = titleElement.innerHTML + " mundo";
+// const titleElement = document.querySelector('.title');
+// titleElement.innerHTML = titleElement.innerHTML + " mundo";
 
-const subtitleElement = document.querySelector('.subtitle');
-subtitleElement.innerHTML = subtitleElement.innerHTML + 'María Somoza';
+// const subtitleElement = document.querySelector('.subtitle');
+// subtitleElement.innerHTML = subtitleElement.innerHTML + 'María Somoza';
 
 const divElement = document.querySelector('.div');
-const content = '<h1>Lorem Ipsum</h1><img src ="http://via.placeholder.com/350x150"/><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>';
-divElement.innerHTML = content;
+
+const content = '<h1>Lorem Ipsum</h1> <img src ="http://via.placeholder.com/350x150"/> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>';
+
+divElement.innerHTML += content;
