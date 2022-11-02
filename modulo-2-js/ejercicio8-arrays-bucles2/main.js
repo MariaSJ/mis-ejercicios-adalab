e175cdf7090633b6422bb65be362519d17006497
Maria SJ <maria.som.jur@gmail.com>
2022-11-02 20:44:29 +0100

    exercise 8 arrays and bucles 2

---
+++ modulo-2-js/ejercicio8-arrays-bucles2/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//ejercicio 1
+
+function get100Numbers() {
+    const arr = [];
+    for (let number = 0; number < 100; number++){
+        console.log(arr[number + 1]);
+    }
+    return arr.push(number);
+};
+
+console.log(get100Numbers);
+
